47648c02cb9916273211016e4beb1a134d991575
krishnawithcoder <krishnasinghjava@gmail.com>
2023-02-13 13:30:51 +0530

    part

---
+++ lib/router.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:whatsapp_ui/common/widgets/error.dart';
+import 'package:whatsapp_ui/features/auth/screen/login_screen.dart';
+import 'package:whatsapp_ui/features/auth/screen/otp_screen.dart';
+import 'features/auth/screen/user_information_screen.dart';
+
+Route<dynamic> generateRoute(RouteSettings settings) {
+  switch (settings.name) {
+    case LoginScreen.routeName:
+      return MaterialPageRoute(
+        builder: (context) => const LoginScreen(),
+      );
+    case OTPScreen.routeName:
+      final verificationId = settings.arguments as String;
+      return MaterialPageRoute(
+        builder: (context) => OTPScreen(
+          verificationId: verificationId,
+        ),
+      );
+    case UserInformationScreen.routeName:
+      return MaterialPageRoute(
+        builder: (context) => const UserInformationScreen(),
+      );
+
+    default:
+      return MaterialPageRoute(
+        builder: (context) => const Scaffold(
+          body: ErrorScreen(error: 'This page doesn\'t exist'),
+        ),
+      );
+  }
+}
