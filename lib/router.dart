dd93fe34750ad23455b20e55947cd3a1b0f7edc2
krishnawithcoder <krishnasinghjava@gmail.com>
2023-02-15 12:24:45 +0530

    p

--- lib/router.dart
+++ lib/router.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:whatsapp_ui/common/widgets/error.dart';
 import 'package:whatsapp_ui/features/auth/screen/login_screen.dart';
 import 'package:whatsapp_ui/features/auth/screen/otp_screen.dart';
+import 'package:whatsapp_ui/screens/mobile_chat_screen.dart';
 import 'features/auth/screen/user_information_screen.dart';
+import 'features/select_contacts/screens/select_contacts_screen.dart';
 
 Route<dynamic> generateRoute(RouteSettings settings) {
   switch (settings.name) {
@@ -21,6 +23,28 @@ Route<dynamic> generateRoute(RouteSettings settings) {
       return MaterialPageRoute(
         builder: (context) => const UserInformationScreen(),
       );
+    case SelectContactsScreen.routeName:
+      return MaterialPageRoute(
+        builder: (context) => const SelectContactsScreen(),
+      );
+    case MobileChatScreen.routeName:
+      return MaterialPageRoute(
+        builder: (context) => const MobileChatScreen(),
+      );
+    //  case MobileChatScreen.routeName:
+    // final arguments = settings.arguments as Map<String, dynamic>;
+    // final name = arguments['name'];
+    // final uid = arguments['uid'];
+    // final isGroupChat = arguments['isGroupChat'];
+    // final profilePic = arguments['profilePic'];
+    // return MaterialPageRoute(
+    //   builder: (context) => MobileChatScreen(
+    //     name: name,
+    //     uid: uid,
+    //     isGroupChat: isGroupChat,
+    //     profilePic: profilePic,
+    //   ),
+    // );
 
     default:
       return MaterialPageRoute(
